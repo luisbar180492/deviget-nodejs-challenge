ba66a01ab89b26ad1f4839b979f07f22c4de0c51
Luis Angel Barrancos Ortiz <luisbar@Luis-Angels-MacBook-Pro.local>
2021-08-14 18:23:31 -0400

    feat: add logic for receiving and forwarding messages

--- index.js
+++ index.js
@@ -19,24 +19,29 @@ const room = {
   ],
 }
 
-io.on('connection', (socket) => {
-  if (room.playerOne && room.playerTwo) return socket.disconnect(true)
-  
-  socket.on('message', (data, ack) => {
-    if (room.playerOne.id === data.id)
-      return room.playerTwo.send(data, () => ack())
-    if (room.playerTwo.id === data.id)
-      return room.playerOne.send(data, () => ack())
+const onMessage = (data, ack) => {
+  if (room.playerOne.id === data.id)
+    return room.playerTwo.send(data, () => ack())
+  if (room.playerTwo.id === data.id)
+    return room.playerOne.send(data, () => ack())
 
-  })
+}
+
+const onDisconnect = (socket) => (reason) => {
+  if (room.playerOne && socket.id === room.playerOne.id) return room.playerOne = undefined
+  if (room.playerTwo && socket.id === room.playerTwo.id) return room.playerTwo = undefined
+}
+
+io.on('connection', (socket) => {
+  if (room.playerOne && room.playerTwo) return socket.emit('full')
+  
+  socket.on('message', onMessage)
+  socket.on('disconnect', onDisconnect(socket))
   
   if (!room.playerOne) return room.playerOne = socket
-
   room.playerTwo = socket
-})
-
-io.on('disconnect', (socket) => {
-  console.log(`disconnected`);
+  room.playerOne.emit('ready', { id: room.playerOne.id })
+  room.playerTwo.emit('ready', { id: room.playerOne.id })
 })
 
 httpServer.listen(PORT, () => console.log(`Server running on ${PORT}`))
